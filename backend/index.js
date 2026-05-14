937eb899b05cc3b0f07cbeff4596e02955002a3b
david rugeles <david.rugeles@pi.edu.co>
2026-05-13 20:47:22 -0500

    creamos rutas en node con express

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,18 @@
 const express = require('express')
-
+let nombre = 'Miguel'
 const app = express()
 
+app.get('/',(solicitud,respuesta)=>{
+
+    respuesta.send('<h1>Hola soy ' +nombre+ '</h1>')
+
+})
+
+app.get('/productos',(solicitud,respuesta)=>{
+
+    respuesta.send('<h1>pagina productos de carrito</h1>')
+
+})
 
 app.listen(3000,()=>{
     console.log('cree un servidor en node y funciona!!')
