a697e6f9a8cf9e6662f17c0ee0cfa0c9ac3431bc
jose <jose.varon@pi.edu.co>
2026-06-14 18:33:36 -0500

    Arreglo el frontend desde Express y ajusto css

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const router = require('./routes/router');
 const cors = require('cors');
+const path = require('path');
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+app.use(express.static(path.join(__dirname, '../frontend')));
 
 app.use('/api', router);
 
