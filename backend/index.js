fa969b224b171649541fdd987dedca93a49cfd6b
Miguel Angel <miguelangelcalderon93@hotmail.com>
2026-06-13 19:36:29 -0500

    Agregamos la funcion fetch

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,10 @@
 const express = require('express')
 const router = require('./routes/router')
 const app = express()
+const cors = require('cors')
 
-app.use('/api',router)
+app.use(cors())
+app.use('/api', router)
 
 app.listen(3000, ()=>{
     console.log("He creado mi primera API\ny Sí FUNCIONA")
--- frontend/js/app.js
+++ frontend/js/app.js
@@ -1,11 +1,22 @@
 let ingresos = document.getElementById("row-ingreso");
+let gastos = document.getElementById("row-gasto");
 
-let titulo = "Sueldo"
-let valor = 2500000
+fetch('http://localhost:3000/api/movimientos')
+.then(datos => datos.json())
+.then((objeto) => {
 
-for(i=0; i<5; i++){
-    ingresos.innerHTML += `<tr>
-                            <td>${titulo + i}</td>
-                            <td>${valor}</td>
+    for(i=0; i<5; i++){
+        let tipoCategoria = objeto.data[i].tipo_categoria;
+        if(tipoCategoria === "ingreso"){
+            ingresos.innerHTML += `<tr>
+                            <td>${objeto.data[i].nombre_categoria}</td>
+                            <td>${objeto.data[i].monto}</td>
                         </tr>`
+        } else if(tipoCategoria === "gasto"){
+            gastos.innerHTML += `<tr>
+                            <td>${objeto.data[i].nombre_categoria}</td>
+                            <td>${objeto.data[i].monto}</td>
+                        </tr>`
+        }
 }
+})
