e0a969305acf48e4abd36c9d1082a5ca38a48059
eileen <eileen.gutierrez@pi.edu.co>
2026-06-14 23:23:40 -0500

    realice la base de datos

---
+++ backend/index.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const router = require('./router')
+const app = express()
+const cors = require('cors')
+
+app.use(cors())
+app.use('/api', router)
+
+app.listen(3000,()=>{
+    console.log('cree un servidor en node y funciona!!')
+})
---
+++ backend/router.js
@@ -0,0 +1,8 @@
+const express = require('express')
+const router = express.Router()
+const controller = require('../controller/gastos.controller')
+
+router.get('/movimiento', controller.consultarGastos)
+router.get('/categoria',controller.obtenerCategoria)
+
+module.exports = routerssss
