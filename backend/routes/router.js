71af402a3ddc3cdcaae511401961b263254159f3
jose <jose.varon@pi.edu.co>
2026-07-01 10:13:36 -0500

    Agrego ruta para obtener resumen de movimientos

---
+++ backend/routes/router.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const router = express.Router()
+
+const controller = require('../controller/gastos.controller')
+
+
+router.get('/movimiento', controller.consultarGastos)
+
+router.get('/categoria', controller.obtenerCategoria)
+
+
+router.post('/movimiento',controller.crearMovimiento)
+
+router.post('/categoria',controller.crearCategoria)
+
+router.get('/resumen', controller.resumen)
+
+
+module.exports = router
